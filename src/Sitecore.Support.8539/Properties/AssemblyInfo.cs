27cd91605d1fd3f7bf007e6c2ef9badb03d96b16
Anton Kechashin <atk@sitecore.net>
2017-07-31 12:41:59 +0300

    Initial commit.

---
+++ src/Sitecore.Support.8539/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.8539")]
+[assembly: AssemblyProduct("Sitecore.Support.8539")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.8539/XA/Foundation/Editing/Pipelines/RenderField/RenderWebEditing.cs
@@ -0,0 +1,34 @@
+﻿namespace Sitecore.Support.XA.Foundation.Editing.Pipelines.RenderField
+{
+    using Sitecore.Pipelines.RenderField;
+    using Sitecore.XA.Foundation.Multisite.Extensions;
+    using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+
+    public class RenderWebEditing : Sitecore.Pipelines.RenderField.RenderWebEditing
+    {
+        public new virtual void Process(RenderFieldArgs args)
+        {
+            if (!Context.Site.IsSxaSite())
+            {
+                #region Added code
+                base.Process(args); 
+                #endregion
+                return;
+            }
+
+            string localContent = args.RenderParameters["local-content"];
+            string disableEditing = args.RenderParameters["disable-editing"];
+
+            if (disableEditing == "true")
+            {
+                if (localContent == "false")
+                {
+                    return;
+                }
+                args.Parameters = args.Parameters.AddParameter("skipcommonbuttons", string.Empty);
+            }
+
+            base.Process(args);
+        }
+    }
+}
